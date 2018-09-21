8c4beeb4b5d520704fc1cc46420696c5ff62c5c7
jessica.lbriano <jessica.lbriano23@gmail.com>
2018-09-20 21:20:40 -0300

    O mundo está ficando cruel! Iniciando meu projeto!

--- Nina02.py
+++ Nina02.py
@@ -7,5 +7,5 @@ y1 = int(input("coordenada y1 de P1: "))
 x2 = int(input("coordenada x2 de P2: "))
 y2 = int(input("coordenada y2 de P2: "))
 
-d= math.sqrt((x1-x2** + (y1-y2)**2)
+d= math.sqrt(x1-x2** + (y1-y2)**2)
 print(" distância entre P1 e P2 é: ", d)
